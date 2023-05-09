67d9d113fa095120704016977e9b82f62ac750b8
diegojacober <diegoalencar.jacober@gmail.com>
2023-05-09 16:59:17 -0300

    Web scrapping

--- web-scraping/main.py
+++ web-scraping/main.py
@@ -1,13 +1,52 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 
-servico = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico)
 
-navegador.get("https://twitter.com/home")
+options = Options()
+options.add_argument('window-size=1000,1800')
+
+navegador = webdriver.Chrome(options=options)
+
+navegador.get("https://www.magazineluiza.com.br/")
+sleep(1.8)
+
+input = navegador.find_element('tag name', 'input')
+input.send_keys("celular samsung")
+sleep(1.5)
+input.submit()
 sleep(2)
-navegador.find_element('xpath', '')
 
-sleep(100000000)
+navegador.refresh()
+
+lista = navegador.find_element('xpath', '/html/body/div[1]/div/main/section[4]/div[3]/div/ul')
+celulares = lista.text
+celulares = celulares.split("\n")
+
+preco = navegador.find_element('xpath', '/html/body/div[1]/div/main/section[4]/div[3]/div/ul/li[1]/a/div[3]/div[2]/div/p')
+valores = preco.text
+valores = valores.split("/n")
+
+count = 0
+count1 = 0
+
+for celular in celulares:
+    if "Smartphone" in celular:
+        if count == 10:
+            break
+        else:
+            print(celular)
+            count += 1
+
+print(valores)    
+#for preco in celulares:
+#    if "R$" in preco:
+#        if count1 == 10:
+#            break
+#        else:
+#            print(preco)
+#            count1 += 1
+
+sleep(100000)
